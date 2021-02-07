e1d04ff8e93ae599d9b595b9c0a86dd2e580534f
Vinson <vinson1493@gmail.com>
2021-02-07 21:35:25 +0800

    Testing gh-pages

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,7 @@ import React from "react";
 const Intro = () => {
     return (
         <div className="col-10">
-            <h1 className="display-2 text-center">Fake Toto</h1>
+            <h1 className="display-2 text-center">Fake Totototo</h1>
             <p className="text-center">
                 This is a prank website to prank your family and friends
                 <br />
